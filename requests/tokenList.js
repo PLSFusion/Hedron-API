0d512bc66ae86ac56f39f39b9261da066f5683d8
Alex McWhirter <alex@triadic.us>
2022-03-23 02:13:43 -0400

    add testnet tokens

--- requests/tokenList.js
+++ requests/tokenList.js
@@ -30,7 +30,55 @@ function get () {
         symbol: 'HDRN',
         decimals: 9,
         logoURI: 'https://hedron.pro/img/token/hdrn/32.png'
-      }
+      },
+      {
+        chainId: 941,
+        address: '0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39',
+        name: 'HEX',
+        symbol: 'HEX',
+        decimals: 8,
+        logoURI: 'https://hedron.pro/img/token/hex/32.png'
+      },
+      {
+        chainId: 941,
+        address: '0x3819f64f282bf135d62168C1e513280dAF905e06',
+        name: 'Hedron',
+        symbol: 'HDRN',
+        decimals: 9,
+        logoURI: 'https://hedron.pro/img/token/hdrn/32.png'
+      },
+      {
+        name: 'USDCoin',
+        address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        symbol: 'USDC',
+        decimals: 6,
+        chainId: 941,
+        logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png'
+      },
+      {
+        name: 'Tether USD',
+        address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+        symbol: 'USDT',
+        decimals: 6,
+        chainId: 941,
+        logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png'
+      },
+      {
+        name: 'Wrapped Pulse',
+        address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+        symbol: 'WPLS',
+        decimals: 18,
+        chainId: 941,
+        logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
+      },
+      {
+        name: 'Dai Stablecoin',
+        address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+        symbol: 'DAI',
+        decimals: 18,
+        chainId: 941,
+        logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xaD6D458402F60fD3Bd25163575031ACDce07538D/logo.png'
+      },
     ]
   };
 }
